daefe853b0da1cc066e8e9b72df00f4d3fede4d8
dgrumm <dgrumm@gmail.com>
2018-11-27 08:29:01 -0800

    added buzz phrase - digital transformation

--- buzz/generator.py
+++ buzz/generator.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import random
 
 buzz = ('continuous delivery', 'continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+    'continuous deployment', 'continuous improvement', 'devops', 'digital transformation')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
