74ccf9d2c18bc8ba57069f43693e3ee5a78b3504
David.Grumm <david.grumm@calculi.com>
2019-09-16 15:10:00 -0700

    GX-0000 adding start script - attempt #1

---
+++ ci/bin/install-and-start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "ECHO environment from inside install-and-start.sh"
+printenv | sort
+echo "=-=-=-=-=-=-=-=-=-="
+
+echo "ECHO file listing"
+ls -lR ${APPLICATION_JOB_DIR}
+cd ${APPLICATION_JOB_DIR}
+echo "=-=-=-=-=-=-=-=-=-="
+
+echo "RUN apt-get update"
+apt-get install -y --no-install-recommends python3-pip
+echo "=-=-=-=-=-=-=-=-=-="
+
+echo "RUN pip install"
+pip3 install -r requirements.txt
+echo "=-=-=-=-=-=-=-=-=-="
+
+echo "START buzzworthy webapp"
+which python3
+python3 webapp.py
+#python webapp.py
