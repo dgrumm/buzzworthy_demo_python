9db67db613d3647ecf88e1ffa102b93f86ec41ce
David.Grumm <david.grumm@calculi.com>
2019-09-17 18:40:51 -0700

    GX-0000 another attempt to install pip

--- ci/bin/install-and-start.sh
+++ ci/bin/install-and-start.sh
@@ -9,42 +9,42 @@ echo "=-=-=-=-=-=-=-=-=-="
 echo "=-=-=-=-=-=-=-=-=-="
 echo "ECHO file listings"
 echo "APPLICATION_JOB_DIR"
-ls -lR ${APPLICATION_JOB_DIR}
+ls -lR "${APPLICATION_JOB_DIR}
 
 echo "APPLICATION_RUN_DIR"
-ls -lR ${APPLICATION_RUN_DIR}
+ls -lR "${APPLICATION_RUN_DIR}"
 
 echo "APPLICATION_DATA_DIR"
-ls -lR ${APPLICATION_DATA_DIR}
+ls -lR "${APPLICATION_DATA_DIR}"
 
 echo "APPLICATION_STORE_DIR"
-ls -lR ${APPLICATION_STORE_DIR}
+ls -lR "${APPLICATION_STORE_DIR}"
 
 echo "PKG_BASE_DIR"
-ls -lR ${PKG_BASE_DIR}
+ls -lR "${PKG_BASE_DIR}"
 
 echo "JOBS_BASE_DIR"
-ls -lR ${JOBS_BASE_DIR}
+ls -lR "${JOBS_BASE_DIR}"
 
 echo "RUN_BASE_DIR"
-ls -lR ${RUN_BASE_DIR}
+ls -lR "${RUN_BASE_DIR}"
 
 echo "LOGS_BASE_DIR"
-ls -lR ${LOGS_BASE_DIR}
+ls -lR "${LOGS_BASE_DIR}"
 echo "=-=-=-=-=-=-=-=-=-="
 
-echo "RUN apt-get update"
-#apt-get install -y --no-install-recommends python3-pip
-echo "=-=-=-=-=-=-=-=-=-="
+#echo "RUN apt-get update"
+#sudo apt-get install -y --no-install-recommends python3-pip
+#echo "=-=-=-=-=-=-=-=-=-="
+echo "Hail Mary"
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py && python get-pip.py --user
+pushd .local/bin
+./pip install -r requirements --user
+popd
 
-echo "SETUP Python Virtual Environment"
-python3 -m venv venv
-. venv/bin/activate
-echo "=-=-=-=-=-=-=-=-=-="
-
-echo "RUN pip install"
-pip install -r requirements.txt
-echo "=-=-=-=-=-=-=-=-=-="
+#echo "RUN pip install"
+#pip3 install -r requirements.txt
+#echo "=-=-=-=-=-=-=-=-=-="
 
 echo "START buzzworthy webapp"
 pushd ${PKG_BASE_DIR}/${APPLICATION_NAME}
