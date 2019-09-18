2eea3bfe183b3e4d2a1c1cf60412e4f1b91d191f
David.Grumm <david.grumm@calculi.com>
2019-09-17 17:54:33 -0700

    GX-0000 updated install script - trying to get python env setup

--- ci/bin/install-and-start.sh
+++ ci/bin/install-and-start.sh
@@ -1,27 +1,53 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-
+echo "=-=-=-=-=-=-=-=-=-="
 echo "ECHO environment from inside install-and-start.sh"
 printenv | sort
 echo "=-=-=-=-=-=-=-=-=-="
 
-echo "ECHO file listing"
+echo "=-=-=-=-=-=-=-=-=-="
+echo "ECHO file listings"
+echo "APPLICATION_JOB_DIR"
 ls -lR ${APPLICATION_JOB_DIR}
-cd ${APPLICATION_JOB_DIR}
+
+echo "APPLICATION_RUN_DIR"
+ls -lR ${APPLICATION_RUN_DIR}
+
+echo "APPLICATION_DATA_DIR"
+ls -lR ${APPLICATION_DATA_DIR}
+
+echo "APPLICATION_STORE_DIR"
+ls -lR ${APPLICATION_STORE_DIR}
+
+echo "PKG_BASE_DIR"
+ls -lR ${PKG_BASE_DIR}
+
+echo "JOBS_BASE_DIR"
+ls -lR ${JOBS_BASE_DIR}
+
+echo "RUN_BASE_DIR"
+ls -lR ${RUN_BASE_DIR}
+
+echo "LOGS_BASE_DIR"
+ls -lR ${LOGS_BASE_DIR}
 echo "=-=-=-=-=-=-=-=-=-="
 
 echo "RUN apt-get update"
 #apt-get install -y --no-install-recommends python3-pip
 echo "=-=-=-=-=-=-=-=-=-="
 
+echo "SETUP Python Virtual Environment"
+python3 -m venv venv
+. venv/bin/activate
+echo "=-=-=-=-=-=-=-=-=-="
+
 echo "RUN pip install"
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 echo "=-=-=-=-=-=-=-=-=-="
 
 echo "START buzzworthy webapp"
-which python3
-
 pushd ${PKG_BASE_DIR}/${APPLICATION_NAME}
 python3 webapp.py
 #python webapp.py
+echo "=-=-=-=-=-=-=-=-=-="
