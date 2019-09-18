2b6e2e7c209eca8c38f049decbcc32839b285016
David.Grumm <david.grumm@calculi.com>
2019-09-18 13:35:37 -0700

    GX-0000 still trying to figure how to install python/pip

--- ci/bin/install-and-start.sh
+++ ci/bin/install-and-start.sh
@@ -2,52 +2,41 @@
 set -exuo pipefail
 
 echo "=-=-=-=-=-=-=-=-=-="
+
 echo "ECHO environment from inside install-and-start.sh"
 printenv | sort
-echo "=-=-=-=-=-=-=-=-=-="
 
 echo "=-=-=-=-=-=-=-=-=-="
-echo "ECHO file listings"
-echo "APPLICATION_JOB_DIR"
-ls -lR "${APPLICATION_JOB_DIR}"
 
-echo "APPLICATION_RUN_DIR"
-ls -lR "${APPLICATION_RUN_DIR}"
+echo "ECHO virtual environment?"
+source ./venv/bin/activate
 
-echo "APPLICATION_DATA_DIR"
-ls -lR "${APPLICATION_DATA_DIR}"
+echo "=-=-=-=-=-=-=-=-=-="
 
-echo "APPLICATION_STORE_DIR"
-ls -lR "${APPLICATION_STORE_DIR}"
+echo "ECHO python3 installed?"
+which python3
+python3 --version
+which pip3
+pip3 --version
 
-echo "PKG_BASE_DIR"
-ls -lR "${PKG_BASE_DIR}"
-
-echo "JOBS_BASE_DIR"
-ls -lR "${JOBS_BASE_DIR}"
-
-echo "RUN_BASE_DIR"
-ls -lR "${RUN_BASE_DIR}"
-
-echo "LOGS_BASE_DIR"
-ls -lR "${LOGS_BASE_DIR}"
 echo "=-=-=-=-=-=-=-=-=-="
 
 #echo "RUN apt-get update"
 #sudo apt-get install -y --no-install-recommends python3-pip
 #echo "=-=-=-=-=-=-=-=-=-="
-echo "Hail Mary"
-curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py && python get-pip.py --user
-pushd .local/bin
-./pip install -r requirements --user
-popd
+#echo "Hail Mary"
+# curl https://bootstrap.pypa.io/3.3/get-pip.py --output get-pip.py && python get-pip.py --user
+# pushd .local/bin
+# ./pip install -r requirements --user
+# popd
 
 #echo "RUN pip install"
 #pip3 install -r requirements.txt
-#echo "=-=-=-=-=-=-=-=-=-="
+echo "=-=-=-=-=-=-=-=-=-="
 
 echo "START buzzworthy webapp"
 pushd ${PKG_BASE_DIR}/${APPLICATION_NAME}
 python3 webapp.py
 #python webapp.py
+
 echo "=-=-=-=-=-=-=-=-=-="
