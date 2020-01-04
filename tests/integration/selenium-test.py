531961f64cef9241292181640b0459d8f3f9d356
donghoang89 <dong.hoang@integnology.com>
2020-01-03 16:04:16 -0800

    GX-4551 templatize tests/integration/selenium-test.py

--- tests/integration/selenium-test.py
+++ tests/integration/selenium-test.py
@@ -1,3 +1,5 @@
+import pytest
+import pytest_html
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
